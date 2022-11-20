3ddcbf223fb85071750f5c57c37b3f1a862a0c9a
Helin <helintoptanci3@outlook.com>
2022-11-20 19:52:51 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+
+const divide = 2000 / 100;
+console.log(divide);
+
+const square = (parameter1) => parameter1 * parameter1;
+console.log(square (2));
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+console.log(add(2,3));
